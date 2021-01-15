ed01be44d76a8c7988f815ef89ac454c3dcbff5c
Allison Bahls <allison_sousa_bahls@hotmail.com>
2021-01-15 18:48:35 -0300

    Add table responsive for reserver

--- markequip-app/src/Home/index.tsx
+++ markequip-app/src/Home/index.tsx
@@ -1,8 +1,10 @@
+import "./styles.css";
 export default function Home() {
     return (
-        <>
-        <div>
+        <div className="main-container">
+            <a className="btn-reserver" href="/#">Reservados</a>
+            <a className="btn-using" href="/#">Em uso</a>
+            <a className="btn-" href="/#">Devolvidos</a>
         </div>
-        </>
     );
 }
