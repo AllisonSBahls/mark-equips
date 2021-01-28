9eb2af1bd2aea218cae793175ed34636061bfc06
Allison Sousa Bahls <allison_sousa_bahls@hotmail.com>
2021-01-27 22:21:43 -0400

    fix api

--- markequip-app/src/Services/Api.tsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
-})
-
-export function auth(login: any){
-   return api.post('api/v1/auth/login', login);
-}
